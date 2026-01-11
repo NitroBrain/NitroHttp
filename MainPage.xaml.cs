e0a5e868020a69275c7c1a743792976fa2ff2721
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-11 20:57:38 +0500

    feat: add async HTTP GET button handler to MainPage
    - Implemented a button click event that triggers an async GET request to the specified URL. Displays response content and status code, with error alerts for empty URLs or request failures.

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -9,5 +9,35 @@
             InitializeComponent();
         }
 
+        private async void Button_Click(object sender, EventArgs e)
+        {
+            await GET(sender, e);
+        }
+        private async Task GET(object sender, EventArgs e)
+        {
+            ArgumentNullException.ThrowIfNull(sender);
+            ArgumentNullException.ThrowIfNull(e);
+
+            if (string.IsNullOrEmpty(url.Text))
+            {
+                _ = DisplayAlert("Error", "Url is Empty", "OK");
+            }
+            else
+            {
+                try
+                {
+                    string respoonseUrl = url.Text;
+                    HttpResponseMessage responseMessage = await _httpClient.GetAsync(respoonseUrl);
+                    string responseText = await responseMessage.Content.ReadAsStringAsync();
+                    response.Text = responseText;
+                    int statusCode = (int)responseMessage.StatusCode;
+                    status.Text = statusCode.ToString();
+                }
+                catch (Exception ex)
+                {
+                    _ = DisplayAlert("Error", ex.Message, "OK");
+                }
+            }
+        }
     }
 }
