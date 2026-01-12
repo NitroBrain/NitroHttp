178fb5356022bff993f4588e82862e8a77cda48f
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-13 01:02:31 +0500

    feat: remove borders from Entry, Editor, Picker on Windows

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using Microsoft.Maui.Handlers;
 
 namespace NitroHttp
 {
@@ -17,6 +18,41 @@ namespace NitroHttp
 
 #if DEBUG
     		builder.Logging.AddDebug();
+#endif
+
+#if WINDOWS
+            EntryHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+                var textBox = handler.PlatformView;
+                textBox.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+                textBox.FocusVisualPrimaryThickness = new Microsoft.UI.Xaml.Thickness(0);
+                textBox.FocusVisualSecondaryThickness = new Microsoft.UI.Xaml.Thickness(0);
+                
+                textBox.Resources["TextControlBorderThemeThickness"] = new Microsoft.UI.Xaml.Thickness(0);
+                textBox.Resources["TextControlBorderThemeThicknessFocused"] = new Microsoft.UI.Xaml.Thickness(0);
+            });
+
+            EditorHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+                var textBox = handler.PlatformView;
+                textBox.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+                textBox.FocusVisualPrimaryThickness = new Microsoft.UI.Xaml.Thickness(0);
+                textBox.FocusVisualSecondaryThickness = new Microsoft.UI.Xaml.Thickness(0);
+                
+                textBox.Resources["TextControlBorderThemeThickness"] = new Microsoft.UI.Xaml.Thickness(0);
+                textBox.Resources["TextControlBorderThemeThicknessFocused"] = new Microsoft.UI.Xaml.Thickness(0);
+            });
+
+            PickerHandler.Mapper.AppendToMapping("NoBorder", (handler, view) =>
+            {
+                var comboBox = handler.PlatformView;
+                comboBox.BorderThickness = new Microsoft.UI.Xaml.Thickness(0);
+                comboBox.FocusVisualPrimaryThickness = new Microsoft.UI.Xaml.Thickness(0);
+                comboBox.FocusVisualSecondaryThickness = new Microsoft.UI.Xaml.Thickness(0);
+                
+                comboBox.Resources["ComboBoxBorderThemeThickness"] = new Microsoft.UI.Xaml.Thickness(0);
+                comboBox.Resources["ComboBoxBorderThemeThicknessFocused"] = new Microsoft.UI.Xaml.Thickness(0);
+            });
 #endif
 
             return builder.Build();
