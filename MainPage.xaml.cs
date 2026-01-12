4a29796b84221d22e9151e0e3be233fa79e38eb5
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-12 10:39:10 +0500

    feat: add POST request method with sample json payload

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace NitroHttp
+﻿using System.Text;
+
+namespace NitroHttp
 {
     public partial class MainPage : ContentPage
     {
@@ -7,11 +9,20 @@
         public MainPage()
         {
             InitializeComponent();
+            MethodPicker.SelectedIndex = MethodPicker.Items.IndexOf("GET");
         }
 
         private async void Button_Click(object sender, EventArgs e)
         {
-            await GET(sender, e);
+            switch (MethodPicker.SelectedItem)
+            {
+                case "GET":
+                    await GET(sender, e);
+                    break;
+                case "POST":
+                    await POST(sender, e);
+                    break;
+            }
         }
         private async Task GET(object sender, EventArgs e)
         {
@@ -30,6 +41,35 @@
                     HttpResponseMessage responseMessage = await _httpClient.GetAsync(respoonseUrl);
                     string responseText = await responseMessage.Content.ReadAsStringAsync();
                     response.Text = responseText;
+                    int statusCode = (int)responseMessage.StatusCode;
+                    status.Text = statusCode.ToString();
+                }
+                catch (Exception ex)
+                {
+                    _ = DisplayAlert("Error", ex.Message, "OK");
+                }
+            }
+        }
+
+        private async Task POST(object sender, EventArgs e)
+        {
+            ArgumentNullException.ThrowIfNull(sender);
+            ArgumentNullException.ThrowIfNull(e);
+
+            if (string.IsNullOrEmpty(url.Text))
+            {
+                _ = DisplayAlert("Error", "Url is Empty", "OK");
+            }
+            else
+            {
+                try
+                {
+                    string responseUrl = url.Text;
+                    string jsonBody = "{\"title\": \"learn HttpClient\", \"body\": \"coding is fun\", \"userId\": 1}";
+                    var content = new StringContent(jsonBody, Encoding.UTF8, "application/json");
+                    HttpResponseMessage responseMessage = await _httpClient.PostAsync(responseUrl, content);
+                    string responseBody = await responseMessage.Content.ReadAsStringAsync();
+                    response.Text = responseBody;
                     int statusCode = (int)responseMessage.StatusCode;
                     status.Text = statusCode.ToString();
                 }
