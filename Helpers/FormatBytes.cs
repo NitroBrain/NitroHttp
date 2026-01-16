db466eb5912158d535f44557fd2c163999ebb5fb
shokhrukhmirzo_usmonov <usmonovshohruxmirzo@gmail.com>
2026-01-16 10:52:34 +0500

    feat(helpers): add FormatBytes helper for byte formatting

---
+++ Helpers/FormatBytes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NitroHttp.Helpers
+{
+    public static class FormatBytes
+    {
+        public static string Format(long bytes)
+        {
+            return bytes switch
+            {
+                < 1024 => $"{bytes}B",
+                < 1024 * 1024 => $"{bytes / 1024.0:F1}KB",
+                _ => $"{bytes / (1024.0 * 1024.0):F1}MB"
+            };
+        }
+    }
+}
