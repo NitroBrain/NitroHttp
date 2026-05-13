68f9f9b591312d3c917c604f50ce43e5ae3711cb
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-13 21:48:40 +0500

    feat(helpers): add format json helper

---
+++ src/NitroHttp.Cli/Helpers/TryFormatJson.cs
@@ -0,0 +1,20 @@
+using System.Text.Json;
+
+namespace Helpers
+{
+  public class FormatJson
+  {
+    public static string TryFormatJson(string text)
+    {
+      try
+      {
+        using var doc = JsonDocument.Parse(text);
+        return JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true });
+      }
+      catch
+      {
+        return text;
+      }
+    }
+  }
+}
