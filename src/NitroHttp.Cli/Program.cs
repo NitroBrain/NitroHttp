dab81fd73539771068f655a3d7e82af8bd875d3a
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-14 23:31:57 +0500

    feat(cli): implement main program structure for NitroHttp CLI

--- src/NitroHttp.Cli/Program.cs
+++ src/NitroHttp.Cli/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.CommandLine;
+using NitroHttp.Cli.Commands;
+using NitroHttp.Cli.Services;
+
+namespace NitroHttp.Cli
+{
+    class Program
+    {
+        static async Task<int> Main(string[] args)
+        {
+            RootCommand rootCommand = new("NitroHttp CLI");
+            var httpService = new HttpService();
+
+            rootCommand.Add(GetCommand.Create(httpService));
+
+            return await rootCommand.Parse(args).InvokeAsync();
+        }
+    }
+}
