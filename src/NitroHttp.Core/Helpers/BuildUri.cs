8c556601dc085fd0611e3868b1c53c291e6eda32
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-16 23:50:55 +0500

    feat(helpers): add build uri

---
+++ src/NitroHttp.Core/Helpers/BuildUri.cs
@@ -0,0 +1,9 @@
+namespace NitroHttp.Core.Helpers;
+
+public static class BuildUri
+{
+  public static Uri Build(string url)
+  {
+    return url.StartsWith("http") ? new Uri(url) : new Uri($"https://{url}");
+  }
+}
