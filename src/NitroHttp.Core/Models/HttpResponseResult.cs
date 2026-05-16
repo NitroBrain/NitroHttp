6111e1dd892a94c1f109839a7dfd3276991eb6ce
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-16 23:51:13 +0500

    feat(models): add http reponse result model

---
+++ src/NitroHttp.Core/Models/HttpResponseResult.cs
@@ -0,0 +1,9 @@
+namespace NitroHttp.Core.Models;
+
+public class HttpResponseResult
+{
+    public string Content { get; set; } = "";
+    public int StatusCode { get; set; }
+    public int Count { get; set; }
+    public int Size { get; set; }
+}
