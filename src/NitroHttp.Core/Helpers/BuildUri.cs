648d95c8207f2f66632f5c60490ef32b12c1f17b
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-19 16:43:54 +0500

    refactor(helpers): improve code stucture

--- src/NitroHttp.Core/Helpers/BuildUri.cs
+++ src/NitroHttp.Core/Helpers/BuildUri.cs
@@ -2,8 +2,8 @@ namespace NitroHttp.Core.Helpers;
 
 public static class BuildUri
 {
-  public static Uri Build(string url)
-  {
-    return url.StartsWith("http") ? new Uri(url) : new Uri($"https://{url}");
-  }
+    public static Uri Build(string url)
+    {
+        return url.StartsWith("http") ? new Uri(url) : new Uri($"https://{url}");
+    }
 }
--- src/NitroHttp.Core/Helpers/TryFormatJson.cs
+++ src/NitroHttp.Core/Helpers/TryFormatJson.cs
@@ -2,18 +2,18 @@ using System.Text.Json;
 
 namespace NitroHttp.Core.Helpers;
 
-  public class FormatJson
-  {
+public class FormatJson
+{
     public static string TryFormatJson(string text)
     {
-      try
-      {
-        using var doc = JsonDocument.Parse(text);
-        return JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true });
-      }
-      catch
-      {
-        return text;
-      }
+        try
+        {
+            using var doc = JsonDocument.Parse(text);
+            return JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true });
+        }
+        catch
+        {
+            return text;
+        }
     }
-  }
+}
