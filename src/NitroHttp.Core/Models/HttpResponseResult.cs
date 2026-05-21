a51ef26300e62d67a9d332fb2f23c36ce2d91078
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-21 23:09:37 +0500

    refactor(http, models): update HttpResponseResult properties

--- src/NitroHttp.Core/Models/HttpResponseResult.cs
+++ src/NitroHttp.Core/Models/HttpResponseResult.cs
@@ -2,8 +2,8 @@ namespace NitroHttp.Core.Models;
 
 public class HttpResponseResult
 {
-    public string? Content { get; set; } = "";
+    public string Content { get; set; } = "";
     public int StatusCode { get; set; }
-    public int Count { get; set; }
-    public int? Size { get; set; }
+    public int Count { get; set; } = 0;
+    public int Size { get; set; } = 0;
 }
