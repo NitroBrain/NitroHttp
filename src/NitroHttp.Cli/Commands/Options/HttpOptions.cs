89c78037c4e766e8c300a8ec4e1620d0335417bf
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-24 22:20:39 +0500

    feat(http): add http options

---
+++ src/NitroHttp.Cli/Commands/Options/HttpOptions.cs
@@ -0,0 +1,19 @@
+using System.CommandLine;
+
+namespace NitroHttp.Cli.Commands.Options
+{
+    public static class HttpOptions
+    {
+        public static Option<string> Url { get; } = new("--url")
+        {
+            Required = true,
+            Description = "The API endpoint URL to send the HTTP request to."
+        };
+
+        public static Option<string> Body { get; } = new("--url")
+        {
+            Required = true,
+            Description = "Request body content to include in the HTTP request."
+        };
+    }
+}
