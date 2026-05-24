69abddc005b9b4b0f837e0564c7c481f7010a0ec
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-05-24 23:02:57 +0500

    refactor(di): register delete cmd and improve code quality

--- src/NitroHttp.Cli/Composition/DependencyInjection.cs
+++ src/NitroHttp.Cli/Composition/DependencyInjection.cs
@@ -1,7 +1,7 @@
 using Microsoft.Extensions.DependencyInjection;
 using NitroHttp.Cli.Commands.Http;
 using NitroHttp.Cli.Commands.Interfaces;
-using NitroHttp.Cli.Views;
+using NitroHttp.Cli.Views.Components;
 using NitroHttp.Cli.Views.Interfaces;
 using NitroHttp.Core.Services;
 using NitroHttp.Core.Services.Interfaces;
@@ -10,15 +10,22 @@ namespace NitroHttp.Cli.Composition;
 
 public static class DependencyInjection
 {
-  public static IServiceProvider Build()
-  {
-    var services = new ServiceCollection();
+    public static IServiceProvider Build()
+    {
+        var services = new ServiceCollection();
 
-    services.AddSingleton<IHttpService, HttpService>();
-    services.AddSingleton<ICommand, GetCommand>();
+        services.AddSingleton<IHttpService, HttpService>();
+        services.AddSingleton<ICommand, GetCommand>();
+        services.AddSingleton<ICommand, PostCommand>();
+        services.AddSingleton<ICommand, PutCommand>();
+        services.AddSingleton<ICommand, PatchCommand>();
+        services.AddSingleton<ICommand, DeleteCommand>();
 
-    services.AddSingleton<IResponseView, ResponseView>();
+        services.AddSingleton<IResponseView, ResponseView>();
+        services.AddSingleton<IResponseStatsView, ResponseStatsView>();
+        services.AddSingleton<IErrorView, ErrorView>();
+        services.AddSingleton<ITable, Table>();
 
-    return services.BuildServiceProvider();
-  }
+        return services.BuildServiceProvider();
+    }
 }
