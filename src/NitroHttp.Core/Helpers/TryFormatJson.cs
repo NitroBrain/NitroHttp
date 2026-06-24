6743bd4825f9343eb859521d933fccce9614effb
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-06-24 15:31:09 +0500

    refactor(helpers): enhance json formatting

--- src/NitroHttp.Core/Helpers/TryFormatJson.cs
+++ src/NitroHttp.Core/Helpers/TryFormatJson.cs
@@ -2,18 +2,24 @@ using System.Text.Json;
 
 namespace NitroHttp.Core.Helpers;
 
-public class FormatJson
+public static class FormatJson
 {
-    public static string TryFormatJson(string text)
+  public static string TryFormatJson(string text)
+  {
+    try
     {
-        try
-        {
-            using var doc = JsonDocument.Parse(text);
-            return JsonSerializer.Serialize(doc, new JsonSerializerOptions { WriteIndented = true });
-        }
-        catch
-        {
-            return text;
-        }
+      using var doc = JsonDocument.Parse(text);
+      using var stream = new MemoryStream();
+      using (var writer = new Utf8JsonWriter(stream, new JsonWriterOptions { Indented = true }))
+      {
+        doc.RootElement.WriteTo(writer);
+      }
+
+      return System.Text.Encoding.UTF8.GetString(stream.ToArray());
     }
+    catch
+    {
+      return text;
+    }
+  }
 }
