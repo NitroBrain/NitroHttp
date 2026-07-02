7005441bb0638fd14083bff8c5c837e4f4457ff3
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-07-02 22:45:20 +0500

    refactor(rendering): replace string based color with enum based

--- src/NitroHttp.Core/Rendering/JsonHighlighter.cs
+++ src/NitroHttp.Core/Rendering/JsonHighlighter.cs
@@ -1,4 +1,5 @@
 using System.Text.RegularExpressions;
+using NitroHttp.Core.Enums;
 
 namespace NitroHttp.Core.Rendering;
 
@@ -19,25 +20,25 @@ public static partial class JsonHighlighter
     {
       return m switch
       {
-        _ when m.Groups["key"].Success => Colorize("cyan", m.Groups["key"].Value),
-        _ when m.Groups["str"].Success => Colorize("green", m.Groups["str"].Value),
-        _ when m.Groups["num"].Success => m.Value[..^m.Groups["num"].Length] + Colorize("orange", m.Groups["num"].Value),
-        _ when m.Groups["bool"].Success => m.Value[..^m.Groups["bool"].Length] + Colorize("purple", m.Groups["bool"].Value),
-        _ when m.Groups["null"].Success => m.Value[..^m.Groups["null"].Length] + Colorize("gray", m.Groups["null"].Value),
+        _ when m.Groups["key"].Success => Colorize(Colors.Cyan, m.Groups["key"].Value),
+        _ when m.Groups["str"].Success => Colorize(Colors.Green, m.Groups["str"].Value),
+        _ when m.Groups["num"].Success => m.Value[..^m.Groups["num"].Length] + Colorize(Colors.Orange, m.Groups["num"].Value),
+        _ when m.Groups["bool"].Success => m.Value[..^m.Groups["bool"].Length] + Colorize(Colors.Purple, m.Groups["bool"].Value),
+        _ when m.Groups["null"].Success => m.Value[..^m.Groups["null"].Length] + Colorize(Colors.Gray, m.Groups["null"].Value),
         _ => m.Value
       };
     });
   }
 
-  public static string Colorize(string color, string text)
+  public static string Colorize(Colors color, string text)
   {
     return color switch
     {
-      "cyan" => Ansi.Cyan + text + Ansi.Reset,
-      "green" => Ansi.Green + text + Ansi.Reset,
-      "orange" => Ansi.Orange + text + Ansi.Reset,
-      "purple" => Ansi.Purple + text + Ansi.Reset,
-      "gray" => Ansi.Gray + text + Ansi.Reset,
+      Colors.Cyan => Ansi.Cyan + text + Ansi.Reset,
+      Colors.Green => Ansi.Green + text + Ansi.Reset,
+      Colors.Orange => Ansi.Orange + text + Ansi.Reset,
+      Colors.Purple => Ansi.Purple + text + Ansi.Reset,
+      Colors.Gray => Ansi.Gray + text + Ansi.Reset,
       _ => text,
     };
   }
