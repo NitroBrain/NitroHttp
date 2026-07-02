0c5e9d9ac69c7c5e9289524321534f3786d83277
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-07-02 23:15:02 +0500

    feat(table): enhance table display with colored HTTP methods and improved formatting

--- src/NitroHttp.Cli/Views/Components/Table.cs
+++ src/NitroHttp.Cli/Views/Components/Table.cs
@@ -1,6 +1,8 @@
 using System.Text;
-using Spectre.Console;
+using System.Text.RegularExpressions;
 using NitroHttp.Cli.Views.Interfaces;
+using NitroHttp.Core.Rendering;
+using NitroHttp.Core.Enums;
 
 namespace NitroHttp.Cli.Views.Components;
 
@@ -9,60 +11,75 @@ public class Table : ITable
     private const int ChunkSize = 500;
     private const int LeftColumnWidth = 8;
 
+    private static readonly Regex AnsiRegex = new(@"\x1B\[[0-9;]*m", RegexOptions.Compiled);
+
     public void Display(string formattedJson, string endpoint = "JSON")
     {
         using var reader = new StringReader(formattedJson);
         var chunk = new List<string>(ChunkSize);
-        const int lineNumber = 0;
-        var windowWidth = Console.WindowWidth;
+        var lineNumber = 0;
 
-        TableHeader(windowWidth, endpoint);
+        TableHeader(Console.WindowWidth, endpoint);
         TableBody(chunk, lineNumber, reader);
         TableBottom();
     }
 
     public static void TableHeader(int windowWidth, string endpoint)
     {
+        var rightWidth = Math.Max(10, windowWidth - (LeftColumnWidth + 5));
         var headerSb = new StringBuilder();
-        var leftHeader = "#".PadRight(LeftColumnWidth);
-        var rightHeader = endpoint.ToUpper().PadRight(windowWidth - 14);
 
-        Console.ForegroundColor = ConsoleColor.Red;
         headerSb.Append('┌')
-          .Append('─', LeftColumnWidth + 2)
-          .Append('┬')
-          .Append('─', Math.Max(10, windowWidth - (LeftColumnWidth + 5)))
-          .Append('┐')
-          .AppendLine();
+            .Append('─', LeftColumnWidth + 2)
+            .Append('┬')
+            .Append('─', rightWidth)
+            .Append('┐')
+            .AppendLine();
+
+        var parts = endpoint.Split(' ', 2);
+        var method = parts[0];
+        var route = parts.Length > 1 ? parts[1] : string.Empty;
+
+        var coloredMethod = method switch
+        {
+            "GET" => JsonHighlighter.Colorize(Colors.Cyan, method),
+            "POST" => JsonHighlighter.Colorize(Colors.Green, method),
+            "PUT" => JsonHighlighter.Colorize(Colors.Yellow, method),
+            "DELETE" => JsonHighlighter.Colorize(Colors.Red, method),
+            _ => method
+        };
+
+        var output = $"{coloredMethod} - {route}";
 
         headerSb.Append('│')
-          .Append(' ')
-          .Append(leftHeader)
-          .Append(' ')
-          .Append('│')
-          .Append(' ')
-          .Append(rightHeader)
-          .Append('│')
-          .AppendLine();
+            .Append(' ')
+            .Append("#".PadRight(LeftColumnWidth))
+            .Append(' ')
+            .Append('│')
+            .Append(' ')
+            .Append(PadRightAnsi(output, rightWidth - 1))
+            .Append('│')
+            .AppendLine();
 
         headerSb.Append('├')
-          .Append('─', LeftColumnWidth + 2)
-          .Append('┼')
-          .Append('─', Math.Max(10, windowWidth - (LeftColumnWidth + 5)))
-          .Append('┤')
-          .AppendLine();
+            .Append('─', LeftColumnWidth + 2)
+            .Append('┼')
+            .Append('─', rightWidth)
+            .Append('┤')
+            .AppendLine();
 
-        Console.Out.Write(headerSb.ToString());
-        Console.ResetColor();
+        Console.Write(headerSb.ToString());
     }
 
     public static void TableBottom()
     {
-        Console.Out.Write('└');
-        Console.Out.Write(new string('─', LeftColumnWidth + 2));
-        Console.Out.Write('┴');
-        Console.Out.Write(new string('─', Math.Max(10, Console.WindowWidth - (LeftColumnWidth + 5))));
-        Console.Out.WriteLine('┘');
+        var rightWidth = Math.Max(10, Console.WindowWidth - (LeftColumnWidth + 5));
+
+        Console.Write('└');
+        Console.Write(new string('─', LeftColumnWidth + 2));
+        Console.Write('┴');
+        Console.Write(new string('─', rightWidth));
+        Console.WriteLine('┘');
     }
 
     public static void TableBody(List<string> chunk, int lineNumber, StringReader reader)
@@ -72,13 +89,15 @@ public class Table : ITable
         while (true)
         {
             var line = reader.ReadLine();
+
             if (line is null)
             {
                 if (chunk.Count > 0)
                 {
                     AppendRows(outSb, chunk, lineNumber - chunk.Count + 1);
-                    Console.Out.Write(outSb.ToString());
+                    Console.Write(outSb.ToString());
                 }
+
                 break;
             }
 
@@ -86,30 +105,56 @@ public class Table : ITable
             lineNumber++;
 
             if (chunk.Count < ChunkSize)
+            {
                 continue;
+            }
 
             AppendRows(outSb, chunk, lineNumber - ChunkSize + 1);
-            Console.Out.Write(outSb.ToString());
+
+            Console.Write(outSb.ToString());
+
             outSb.Clear();
             chunk.Clear();
         }
     }
 
-
     public static void AppendRows(StringBuilder sb, List<string> chunk, int startLine)
     {
+        var rightWidth = Console.WindowWidth - LeftColumnWidth - 7;
+
         for (var i = 0; i < chunk.Count; i++)
         {
             var num = (startLine + i).ToString().PadLeft(LeftColumnWidth);
-            var escaped = Markup.Escape(chunk[i]);
+            var highlighted = JsonHighlighter.Highlight(chunk[i]);
+            var lineNumber = JsonHighlighter.Colorize(Colors.Orange, num);
+
             sb.Append('│')
               .Append(' ')
-              .Append(num)
+              .Append(lineNumber)
               .Append(' ')
               .Append('│')
               .Append(' ')
-              .Append(escaped)
+              .Append(PadRightAnsi(highlighted, rightWidth))
+              .Append(' ')
+              .Append('│')
               .AppendLine();
         }
+    }
+
+    private static int VisibleLength(string text)
+    {
+        return AnsiRegex.Replace(text, "").Length;
+    }
+
+    private static string PadRightAnsi(string text, int width)
+    {
+        var visible = VisibleLength(text);
+
+        if (visible >= width)
+        {
+            return text;
+        }
+
+        return text + new string(' ', width - visible);
     }
 }
