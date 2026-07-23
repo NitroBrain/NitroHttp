ca3a3cd7cd388d002a6c1cbca607df995326ba5b
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-07-23 20:58:38 +0500

    feat(core): add http header model

---
+++ src/NitroHttp.Core/Models/HttpHeader.cs
@@ -0,0 +1,8 @@
+namespace NitroHttp.Core.Models
+{
+    public class HttpHeader
+    {
+        public required string Name { get; init; }
+        public required string Value { get; init; }
+    }
+}
