d6b4bd2d7eff66735d9140d6b2ee74a8738376ca
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-09-08 18:00:13 +0500

    refactor(post): improve request handling and add headers support

--- src/NitroHttp.Cli/Commands/Http/PostCommand.cs
+++ src/NitroHttp.Cli/Commands/Http/PostCommand.cs
@@ -1,8 +1,10 @@
-using NitroHttp.Core.Services.Interfaces;
-using NitroHttp.Cli.Commands.Interfaces;
 using System.CommandLine;
-using NitroHttp.Cli.Views.Interfaces;
+using NitroHttp.Cli.Commands.Interfaces;
 using NitroHttp.Cli.Commands.Options;
+using NitroHttp.Cli.Views.Interfaces;
+using NitroHttp.Core.Helpers;
+using NitroHttp.Core.Models;
+using NitroHttp.Core.Services.Interfaces;
 
 namespace NitroHttp.Cli.Commands.Http;
 
@@ -12,11 +14,11 @@ namespace NitroHttp.Cli.Commands.Http;
 /// <param name="httpService">The HTTP service used to execute the request.</param>
 /// <param name="responseView">The view used to render successful responses.</param>
 /// <param name="errorView">The view used to render errors.</param>
-public class PostCommand(
+public sealed class PostCommand(
     IHttpService httpService,
     IResponseView responseView,
     IErrorView errorView
-  ) : ICommand
+) : ICommand
 {
     /// <summary>
     /// Creates the configured command.
@@ -25,39 +27,46 @@ public class PostCommand(
     public Command Build()
     {
         var command = new Command("post", "Send an HTTP POST request to create a resource.");
+
         command.Aliases.Add("p");
 
         command.Add(HttpOptions.Url);
         command.Add(HttpOptions.Body);
+        command.Add(HttpOptions.Headers);
 
         command.SetAction(async result =>
         {
-            var url = result.GetValue(HttpOptions.Url)!;
-            var body = result.GetValue(HttpOptions.Body)!;
             try
             {
-                if (File.Exists(body))
-                {
-                    body = await File.ReadAllTextAsync(body);
-                }
+                var url = result.GetValue(HttpOptions.Url)!;
+                var body = await InputReader.ReadAsync(result.GetValue(HttpOptions.Body));
+                var headers = await InputReader.ReadAsync(result.GetValue(HttpOptions.Headers));
 
-                var request = await httpService.PostAsync(url, body);
+                var request = new HttpRequestModel
+                {
+                    Method = HttpMethod.Post,
+                    Url = url,
+                    Body = body,
+                    Headers = HeaderParser.Parse(headers)
+                };
+
+                var response = await httpService.ExecuteAsync(request);
+
                 responseView.Display(
-                  $"POST {url}",
-                  request.Content,
-                  request.StatusCode,
-                  request.Count,
-                  request.Size
+                    $"{request.Method} {request.Url}",
+                    response.Content,
+                    response.StatusCode,
+                    response.Count,
+                    response.Size,
+                    response.Headers
                 );
             }
             catch (Exception ex)
             {
                 errorView.Display(ex.Message);
             }
-
         });
 
         return command;
     }
-
 }
