58642402c1118730a693abe0e8c4a485e3fc932e
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-09-08 15:12:19 +0500

    feat: add ICookies interface

---
+++ src/NitroHttp.Cli/Views/Interfaces/ICookies.cs
@@ -0,0 +1,13 @@
+namespace NitroHttp.Cli.Views.Interfaces
+{
+    /// <summary>
+    /// Displays request/response cookies
+    /// </summary>
+    public interface ICookies
+    {
+        /// <summary>
+        /// Displays formmated cookies
+        /// </summary>
+        void Display();
+    }
+}
